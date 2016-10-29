5b38996287e915aabeae1dd406c9205c7803136b
Björn Grüning <bjoern@gruenings.eu>
2016-10-29 20:32:42 +0200

    Fix autoconf

--- recipes/sailfish/build.sh
+++ recipes/sailfish/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# fix autoconf
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/autom4te
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/autoheader 
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/autoreconf 
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/ifnames 
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/autoscan 
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' bin/autoupdate
+
 mkdir -p build
 sed -i 's/Boost_USE_STATIC_LIBS ON/Boost_USE_STATIC_LIBS OFF/' CMakeLists.txt
 cd build
